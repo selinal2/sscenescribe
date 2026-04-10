e81f15fe627ae3b749cf5190ecb687de897454bb
selinal2 <70663347+selinal2@users.noreply.github.com>
2026-04-10 05:30:20 +0000

    Redeploy

--- pages/api/ask.js
+++ pages/api/ask.js
@@ -1,3 +1,4 @@
+// Redeploy trigger
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
